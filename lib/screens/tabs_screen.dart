1cef71b2dcd475a37efc8ae33b4c175f2b4366a1
Ivan <cocahonka@gmail.com>
2023-11-10 10:37:11 +0300

    added bottom navigation bar

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meals_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  var _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String activePageTitle;
+    final Widget activePage;
+
+    switch (_selectedPageIndex) {
+      case 1:
+        activePageTitle = 'Your favourites';
+        activePage = const MealsScreen(title: 'Favourites', meals: []);
+      default:
+        activePageTitle = 'Categories';
+        activePage = const CategoriesScreen();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favourites'),
+        ],
+      ),
+    );
+  }
+}
