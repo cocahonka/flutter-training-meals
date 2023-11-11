e6b914fdeb2deae16d1fbb18c6544c9856b031b7
Ivan <cocahonka@gmail.com>
2023-11-11 23:04:20 +0300

    return data when leaving a filters screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/filter.dart';
 import 'package:meals/widgets/filter_switch.dart';
 
 class FiltersScreen extends StatefulWidget {
@@ -43,33 +44,46 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      body: Column(
-        children: [
-          FilterSwitch(
-            value: _glutenFreeFilterSet,
-            onChanged: _glutenFreeFilterChange,
-            title: 'Gluten-free',
-            subtitle: 'Only included gluten-free meals.',
-          ),
-          FilterSwitch(
-            value: _lactoseFreeFilterSet,
-            onChanged: _lactoseFreeFilterChange,
-            title: 'Lactose-free',
-            subtitle: 'Only included lactose-free meals.',
-          ),
-          FilterSwitch(
-            value: _vegetarianFreeFilterSet,
-            onChanged: _vegetarianFreeFilterChange,
-            title: 'Vegeterian',
-            subtitle: 'Only included vegeterian meals.',
-          ),
-          FilterSwitch(
-            value: _veganFilterSet,
-            onChanged: _veganFilterChange,
-            title: 'Vegan',
-            subtitle: 'Only included vegan meals.',
-          ),
-        ],
+      body: WillPopScope(
+        onWillPop: () async {
+          // ignore: omit_local_variable_types
+          final FiltersStatus status = (
+            gluttenFree: _glutenFreeFilterSet,
+            lactoseFree: _lactoseFreeFilterSet,
+            vegeterian: _vegetarianFreeFilterSet,
+            vegan: _veganFilterSet,
+          );
+          Navigator.of(context).pop(status);
+          return false;
+        },
+        child: Column(
+          children: [
+            FilterSwitch(
+              value: _glutenFreeFilterSet,
+              onChanged: _glutenFreeFilterChange,
+              title: 'Gluten-free',
+              subtitle: 'Only included gluten-free meals.',
+            ),
+            FilterSwitch(
+              value: _lactoseFreeFilterSet,
+              onChanged: _lactoseFreeFilterChange,
+              title: 'Lactose-free',
+              subtitle: 'Only included lactose-free meals.',
+            ),
+            FilterSwitch(
+              value: _vegetarianFreeFilterSet,
+              onChanged: _vegetarianFreeFilterChange,
+              title: 'Vegeterian',
+              subtitle: 'Only included vegeterian meals.',
+            ),
+            FilterSwitch(
+              value: _veganFilterSet,
+              onChanged: _veganFilterChange,
+              title: 'Vegan',
+              subtitle: 'Only included vegan meals.',
+            ),
+          ],
+        ),
       ),
     );
   }
