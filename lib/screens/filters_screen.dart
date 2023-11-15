b2039bf4beebaeae27c8e3910ad3f835f0d0a81a
Ivan <cocahonka@gmail.com>
2023-11-15 23:24:02 +0300

    convert filters screen to stateless widget

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -3,53 +3,42 @@ import 'package:meals/models/filter.dart';
 import 'package:meals/scopes/filters_scope.dart';
 import 'package:meals/widgets/filter_switch.dart';
 
-class FiltersScreen extends StatefulWidget {
+class FiltersScreen extends StatelessWidget {
   const FiltersScreen({super.key});
 
-  @override
-  State<FiltersScreen> createState() => _FiltersScreenState();
-}
-
-class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      body: WillPopScope(
-        onWillPop: () async {
-          Navigator.of(context).pop();
-          return false;
-        },
-        child: Column(
-          children: [
-            FilterSwitch(
-              getValue: (context) => FiltersStateScope.watch(context, Filter.gluttenFree).gluttenFree,
-              onChanged: FiltersStateProvider.of(context).changeGluten,
-              title: 'Gluten-free',
-              subtitle: 'Only included gluten-free meals.',
-            ),
-            FilterSwitch(
-              getValue: (context) => FiltersStateScope.watch(context, Filter.lactoseFree).lactoseFree,
-              onChanged: FiltersStateProvider.of(context).changeLactose,
-              title: 'Lactose-free',
-              subtitle: 'Only included lactose-free meals.',
-            ),
-            FilterSwitch(
-              getValue: (context) => FiltersStateScope.watch(context, Filter.vegeterian).vegeterian,
-              onChanged: FiltersStateProvider.of(context).changeVegeterian,
-              title: 'Vegeterian',
-              subtitle: 'Only included vegeterian meals.',
-            ),
-            FilterSwitch(
-              getValue: (context) => FiltersStateScope.watch(context, Filter.vegan).vegan,
-              onChanged: FiltersStateProvider.of(context).changeVegan,
-              title: 'Vegan',
-              subtitle: 'Only included vegan meals.',
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          FilterSwitch(
+            getValue: (context) => FiltersStateScope.watch(context, Filter.gluttenFree).gluttenFree,
+            onChanged: FiltersStateProvider.of(context).changeGluten,
+            title: 'Gluten-free',
+            subtitle: 'Only included gluten-free meals.',
+          ),
+          FilterSwitch(
+            getValue: (context) => FiltersStateScope.watch(context, Filter.lactoseFree).lactoseFree,
+            onChanged: FiltersStateProvider.of(context).changeLactose,
+            title: 'Lactose-free',
+            subtitle: 'Only included lactose-free meals.',
+          ),
+          FilterSwitch(
+            getValue: (context) => FiltersStateScope.watch(context, Filter.vegeterian).vegeterian,
+            onChanged: FiltersStateProvider.of(context).changeVegeterian,
+            title: 'Vegeterian',
+            subtitle: 'Only included vegeterian meals.',
+          ),
+          FilterSwitch(
+            getValue: (context) => FiltersStateScope.watch(context, Filter.vegan).vegan,
+            onChanged: FiltersStateProvider.of(context).changeVegan,
+            title: 'Vegan',
+            subtitle: 'Only included vegan meals.',
+          ),
+        ],
       ),
     );
   }
